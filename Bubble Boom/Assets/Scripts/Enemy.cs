cf76536e7ee24835b9a1ee14f65ac7bf73277f26
Muhammad Andhika Rizq <mandhikarizq@gmail.com>
2025-02-25 22:44:14 +0700

    Navmesh Test

---
+++ Bubble Boom/Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Transform target; // Target tujuan (misalnya pemain)
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (target != null)
+        {
+            agent.SetDestination(target.position); // Mengatur tujuan agent ke posisi target
+        }
+    }
+}
