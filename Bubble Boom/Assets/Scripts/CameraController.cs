067a884784916e0a8a5b8f81af2bc6463ff2beeb
Muhammad Andhika Rizq <mandhikarizq@gmail.com>
2025-03-16 21:07:08 +0700

    Camera Rotation

--- Bubble Boom/Assets/Scripts/CameraController.cs
+++ Bubble Boom/Assets/Scripts/CameraController.cs
@@ -1,26 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private float movementSpeed = 120;
 
+
+    [Header ("Rotation")]
+    [SerializeField] private Transform focusPoint;
+    [SerializeField] private float maxfocusPointDistance = 15;
+    [SerializeField] private float roationSpeed = 120;
+    [Space]
+    private float pitch;
+    [SerializeField] private float minPitch = 5f;
+    [SerializeField] private float maxPitch = 85f;
+
     private float smoothTime = .1f;
     private Vector3 movementVelocity = Vector3.zero;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
 
     // Update is called once per frame
     void Update()
     {
+        HandleRotation();
         HandleMovement();
+
+        focusPoint.position = transform.position + (transform.forward * GetFocusDistance());
+       
     }
 
-    public void HandleMovement()
+    private float GetFocusDistance()
+    {
+        if(Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, maxfocusPointDistance))
+            return hit.distance;
+        
+        return maxfocusPointDistance;
+
+    }
+
+    private void HandleRotation()
+    {
+        if(Input.GetMouseButton(1))
+        {
+            float horizontalRotation = Input.GetAxis("Mouse X") * roationSpeed * Time.deltaTime;
+            float verticalRotation = Input.GetAxis("Mouse Y") * roationSpeed * Time.deltaTime;
+
+            pitch = Mathf.Clamp(pitch - verticalRotation, minPitch, maxPitch);
+
+            transform.RotateAround(focusPoint.position, Vector3.up, horizontalRotation);
+            transform.RotateAround(focusPoint.position, transform.right, pitch - transform.eulerAngles.x);
+
+            transform.LookAt(focusPoint);
+        }
+    }
+
+    private void HandleMovement()
     {
         Vector3 targetPosition = transform.position;
 
