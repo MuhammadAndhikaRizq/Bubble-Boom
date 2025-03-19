64989942bc6ab9b73988bbade678c7eaaeb0038a
Muhammad Andhika Rizq <mandhikarizq@gmail.com>
2025-03-19 21:21:16 +0700

    Button ChangScale

--- Bubble Boom/Assets/Scripts/UI Manager/UI.cs
+++ Bubble Boom/Assets/Scripts/UI Manager/UI.cs
@@ -6,7 +6,15 @@ using UnityEngine;
 public class UI : MonoBehaviour
 {
    [SerializeField] private GameObject[] uiElements;
+   [SerializeField] private GameObject startUI;
    
+    private void Start()
+    {
+        Time.timeScale = 0; // Pause game saat pertama kali dijalankan
+        if (startUI != null)
+            startUI.SetActive(true);
+    }
+
    public void SwitchTo(GameObject uiEnabled)
    {
         foreach (GameObject ui in uiElements)
@@ -15,6 +23,14 @@ public class UI : MonoBehaviour
         }
         uiEnabled.SetActive(true);
    }
+
+    public void PlayButton()
+    {
+        Time.timeScale = 1; // Melanjutkan permainan
+        if (startUI != null)
+            startUI.SetActive(false);
+      
+    }
 
    public void QuitButton()
    {
--- Bubble Boom/Assets/Scripts/UI Manager/UI_BuildButtons.cs
+++ Bubble Boom/Assets/Scripts/UI Manager/UI_BuildButtons.cs
@@ -6,28 +6,45 @@ using UnityEngine;
 public class UI_BuildButtons : MonoBehaviour
 {
     [SerializeField] private float yPositionOffset;
+    [SerializeField] private float openAnimationDuration = .1f;
 
-    private bool isActive;
+    private UI_BuildButtonOnEffect[] buildButtons;
+
+    private bool isBuildMenuActive;
     private UI_Animator uiAnim;
 
     private void Awake()
     {
         uiAnim = GetComponentInParent<UI_Animator>();
+        buildButtons = GetComponentsInChildren<UI_BuildButtonOnEffect>();
     }
 
+   
     private void Update()
     {
         if(Input.GetKeyDown(KeyCode.B))
             ShowBuildButtons();
     }
 
+
     private void ShowBuildButtons()
     {
-        isActive = !isActive;
+        isBuildMenuActive = !isBuildMenuActive;
 
-        float yOffset = isActive ? yPositionOffset : -yPositionOffset;
+        float yOffset = isBuildMenuActive ? yPositionOffset : -yPositionOffset;
+        float methodDelay = isBuildMenuActive ? openAnimationDuration : 0;
+
         Vector3 offset = new Vector3(0, yOffset);
 
-        uiAnim.ChangePosition(transform, offset);
+        uiAnim.ChangePosition(transform, new Vector3(0, yOffset), openAnimationDuration);
+        Invoke(nameof(ToggleButtonMovement), methodDelay);
+    }
+
+    private void ToggleButtonMovement()
+    {
+        foreach(var button in buildButtons)
+        {
+            button.ToggleMovement(isBuildMenuActive);
+        }
     }
 }
